e8f71a64d2aaf29502c6d0abcb711b1abac42545
henriqueespadas <henriqueep@hotmail.com>
2023-09-17 17:37:15 -0300

    update debit, and credit

--- coin_conversion/models/account_move.py
+++ coin_conversion/models/account_move.py
@@ -2,24 +2,28 @@ from odoo import api, fields, models
 
 
 class AccountMove(models.Model):
-    _inherit = 'account.move'
-
-    currency_id = fields.Many2one('res.currency', string='Currency')
+    _inherit = "account.move"
 
     @api.model
     def create(self, vals):
         record = super(AccountMove, self).create(vals)
 
         if record.currency_id and record.currency_id != record.company_id.currency_id:
-            exchange_rate = self.env['account.foreign.exchange.rate'].search([
-                ('currency_from_id', '=', record.currency_id.id),
-                ('currency_to_id', '=', record.company_id.currency_id.id),
-            ], limit=1, order='date desc')
+            exchange_rate = self.env["account.foreign.exchange.rate"].search(
+                [
+                    ("currency_from_id", "=", record.currency_id.id),
+                    ("currency_to_id", "=", record.company_id.currency_id.id),
+                ],
+                limit=1,
+                order="date desc",
+            )
 
             if exchange_rate:
                 for line in record.line_ids:
-                    original_amount = line.amount_currency
-                    converted_amount = original_amount * exchange_rate.exchange_rate
-                    line.write({'amount_currency': converted_amount})
+                    debit = line.debit
+                    credit = line.credit
+                    converted_debit = debit * exchange_rate.exchange_rate
+                    converted_credit = credit * exchange_rate.exchange_rate
+                    line.write({"debit": converted_debit, "credit": converted_credit})
 
         return record
